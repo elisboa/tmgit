22e761a00ff7a3488ff5777697fc04d40125ba88
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2019-03-05 13:00:21 -0300

    estruturação do flight-mode.sh

---
+++ flight-mode.sh
@@ -0,0 +1,19 @@
+# Verificar ambiente
+function fm_preflight() {
+
+}
+
+# Preparar ambiente
+function fm_climb() {
+
+}
+
+# Executar o código
+function fm_fly() {
+
+}
+
+# Encerrar operações
+function fm_land() {
+
+}

4657387b27c0e2e5284847fd5ffd3226066a15d1
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2019-03-05 13:21:24 -0300

    adicionado código da função fm_preflight

--- flight-mode.sh
+++ flight-mode.sh
@@ -1,6 +1,22 @@
 # Verificar ambiente
 function fm_preflight() {
 
+    ## Inicializar variáveis
+    # variáveis relacionadas ao binário git
+    TMGIT_DIR="${1}"
+    TMGIT_TREE="${2}"
+	TMGIT_GIT="$(which git)"
+    TMGIT_ARGS="--git-dir ${TMGIT_DIR}/.dotfiles/.git --work-tree ${TMGIT_TREE}"
+
+    # variáveis utilizadas pelo git parametrizado (tmgit)
+    # Check which branch we are
+	CUR_BRANCH="IT SHOULD BE A PATTERN OF YYYY.MM.DD"
+    TODAY_DATE="MUST BE YEAR.MONTH.DAY USING DATE COMMAND"
+    COMMIT_DATE="SHOULD BE YEAR.MONTH.DAY.HOUR.MINUTE.SECOND USING DATE COMMAND"
+    # Force current language to C, so all git messages are in default english
+    LANG="C"
+
+
 }
 
 # Preparar ambiente

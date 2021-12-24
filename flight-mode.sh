8aa5bea2004739c6a02bce3a724f614fc0d8cd04
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2021-12-24 13:22:40 -0300

    Feat/silenciar msgs git (#5)
    * :tada: Starting new branch feat/silenciar_msgs_git
    
    * :mute: Remove erro do comando 'command -v git'
    
    * :mute: Suprime mensagens do comando 'git init'
    
    * :mute: Suprime mensagens do comando 'grep ^gitdir'
    
    * :pencil2: Melhora mensagem sobre o comando 'cat .gitignore'
    
    * :pencil2: Corrige a supressão de mensagens do comando 'grep ^gitdir'
    
    * :mute: Suprime mensagens do comando 'git init'
    
    * :pencil2: Melhora mensagem sobre o comando 'cat .gitignore'
    
    * :pencil2: Melhora mensagem sobre verificação do arquivo '.gitignore'
    
    * :bug: Corrige tratamento das variaveis '' e ''
    
    * :bug: Corrige tratamento das variaveis '$LAND_ERRMSG' e '$LAND_ERRLVL'
    
    * :wrench: Melhora verificação do arquivo '.gitignore'
    
    * :rocket: Adiciona verificacao do estado do repositório (git status)
    
    Co-authored-by: Eduardo Lisboa <edlisboa@uolinc.com>

--- flight-mode.sh
+++ flight-mode.sh
@@ -47,7 +47,7 @@ function fm_preflight() {
     # O segundo argumento passado deve ser o diretório ".git" do repositório 
     export TMGIT_DIR="${2}"
     # Pegando o caminho do binário do git
-	TMGIT_GIT="$(command -v git)"
+	TMGIT_GIT="$(command -v git 2> /dev/null)"
     export TMGIT_GIT
     # Montando os parâmetros passados para o GIT
     export TMGIT_ARGS="--git-dir ${TMGIT_DIR} --work-tree ${TMGIT_TREE}"
@@ -125,9 +125,8 @@ function fm_preflight() {
     LAND_ERRLVL=0
     LAND_MSG="Git sendo executado como: ${TMGIT}"
 
-    fm_climb "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}"
-    
-    fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}"
+    fm_climb "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
+    fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
 
 }
 
@@ -151,57 +150,59 @@ function fm_climb() {
         #fi
         
         LAND_MSG="Inicialização do diretório ${TMGIT_TREE} tendo como diretório de controle o ${TMGIT_DIR}"
-        if git init "${TMGIT_TREE}" --separate-git-dir "${TMGIT_DIR}"
+        if git init "${TMGIT_TREE}" --separate-git-dir "${TMGIT_DIR}" 2>&1 > /dev/null
         then
             LAND_ERRMSG="Diretorio ${TMGIT_DIR} inicializado com sucesso"
         else
-            if [[ -e "${TMGIT_TREE}/.git" ]]  && grep ^gitdir "${TMGIT_TREE}/.git"
+            if [[ -e "${TMGIT_TREE}/.git" ]]  && grep ^gitdir "${TMGIT_TREE}/.git" 2>&1 > /dev/null
             then
                 let LAND_ERRLVL+=1
                 LAND_ERRMSG="Diretorio referenciado no arquivo ${TMGIT_TREE}/.git porem inexistente no disco"
-                fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+                fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
             else
                 if [[ -e "${TMGIT_TREE}"/.git ]]
                 then
                     let LAND_ERRLVL+=1
                     LAND_ERRMSG="Entrada para o diretorio ${TMGIT_DIR} inexistente no arquivo ${TMGIT_TREE}/.git"
-                    fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+                    fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
                 fi
             fi
             let LAND_ERRLVL+=1
             LAND_ERRMSG="Falha ao inicializar o diretorio ${TMGIT_DIR}"
-            fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+            fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
         fi
 
-        LAND_MSG="Adicionando arquivo .gitignore"
-        if [[ -e "${TMGIT_TREE}/.gitignore" ]]
+        LAND_MSG="Verificando arquivo .gitignore"
+        if [[ -e "${TMGIT_TREE}/.gitignore" ]] && [[ ! -d "${TMGIT_TREE}/.gitignore" ]]
         then
-            LAND_ERRMSG="Arquivo .gitignore já encontrando"
-            cat "${TMGIT_TREE}/.gitignore"
+            LAND_ERRMSG="Arquivo .gitignore já encontrado."
         else
-            if echo "*" > "${TMGIT_TREE}/.gitignore"
+            if [[ ! -d "${TMGIT_TREE}/.gitignore" ]] && echo "*" > "${TMGIT_TREE}/.gitignore" 2> /dev/null > /dev/null 
             then
                 LAND_ERRMSG="Adicionado arquivo .gitignore com sucesso"
             else
                 let LAND_ERRLVL+=1
                 LAND_ERRMSG="Falha ao adicionar arquivo .gitignore"
-                fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+                fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
             fi
 
         fi
 
+        LAND_MSG="Verificando estado do repositorio"
         # Tudo bem falhar nos comandos acima. Mas o git status não pode falhar!
         # Se ele falhar, aí sim a gente chama a função fm_land
-        if git --git-dir "${TMGIT_DIR}" --work-tree "${TMGIT_TREE}" status
+        if git --git-dir "${TMGIT_DIR}" --work-tree "${TMGIT_TREE}" status 2>&1 > /dev/null
         then
-            echo Tudo bem in the rain
+            LAND_ERRMSG="Repositorio OK"
         else
-            fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+            let LAND_ERRLVL+=1
+            LAND_ERRMSG="Falha ao verificar estado do repositorio"
+            fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
         fi
     else
         let LAND_ERRLVL+=1
         LAND_ERRMSG="Falha ao acessar diretório ${TMGIT_DIR}"
-        fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}" "${LAND_ERRLVL}"
+        fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
     fi
 
 }

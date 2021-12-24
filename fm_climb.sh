0aeb36551f8eb19da3c41e103107a7359b20e395
Eduardo Lisboa <eduardo.lisboa@gmail.com>
2021-12-24 17:32:04 -0300

    :wrench: Corrige criação do arquivo .gitignore e ajusta chamadas de funções

--- fm_climb.sh
+++ fm_climb.sh
@@ -1,5 +1,7 @@
 function init-repo() {
 
+    LAND_CALLER="${LAND_CALLER} -> init-repo"
+
     LAND_MSG="Inicialização do diretório ${TMGIT_TREE} tendo como diretório de controle o ${TMGIT_DIR}"
     if git init "${TMGIT_TREE}" --separate-git-dir "${TMGIT_DIR}" 2>&1 > /dev/null
     then
@@ -26,23 +28,34 @@ function init-repo() {
 
 function check-gitignore() {
 
+    LAND_CALLER="${LAND_CALLER} -> check-gitignore"
+
     LAND_MSG="Verificando arquivo .gitignore"
     if [[ -e "${TMGIT_TREE}/.gitignore" ]] && [[ ! -d "${TMGIT_TREE}/.gitignore" ]]
     then
         LAND_ERRMSG="Arquivo .gitignore já encontrado."
     else
-        if [[ ! -d "${TMGIT_TREE}/.gitignore" ]] && echo "*" > "${TMGIT_TREE}/.gitignore" 2> /dev/null > /dev/null 
+        if [[ ! -d "${TMGIT_TREE}/.gitignore" ]] 
         then
-            LAND_ERRMSG="Adicionado arquivo .gitignore com sucesso"
+            if echo "*" > "${TMGIT_TREE}/.gitignore" 2> /dev/null
+            then
+                LAND_ERRMSG="Adicionado arquivo .gitignore com sucesso"
+            else
+               let LAND_ERRLVL+=1
+               LAND_ERRMSG="Falha ao adicionar arquivo .gitignore"
+               fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"    
+            fi
         else
             let LAND_ERRLVL+=1
-            LAND_ERRMSG="Falha ao adicionar arquivo .gitignore"
+            LAND_ERRMSG="Existe um diretorio no caminho ${TMGIT_TREE}/gitignore e deveria ser um arquivo"
             fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
         fi
     fi
 }
 
 function check-status() {
+
+    LAND_CALLER="${LAND_CALLER} -> check-status"
 
     LAND_MSG="Verificando estado do repositorio"
     # Tudo bem falhar nos comandos acima. Mas o git status não pode falhar!
@@ -59,14 +72,16 @@ function check-status() {
 
 function create-repo() {
 
+    LAND_CALLER="${LAND_CALLER} -> create-repo"
+
     # Tentando criar o diretorio onde a maquina do tempo seria versionada (o git dir)
     LAND_MSG="Verificacao do diretorio de controle do TMGIT em ${TMGIT_DIR}"
     if [[ -d "${TMGIT_TREE}" ]]
     then
         
-        init-repo
-        check-gitignore
-        check-status
+        init-repo "$@"
+        check-gitignore "$@"
+        check-status "$@"
     else
         let LAND_ERRLVL+=1
         LAND_ERRMSG="Falha ao acessar diretório ${TMGIT_DIR}"
@@ -81,5 +96,7 @@ function fm_climb() {
     LAND_CALLER="$2"
     LAND_MSG="$3"
 
-    create-repo
+    LAND_CALLER="${LAND_CALLER} -> fm_climb"
+
+    create-repo "$@"
 }

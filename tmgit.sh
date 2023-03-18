5ed1e0bfff05d3c50c3c0b2202b09765d4b5edc5
Eduardo Lisboa <eduardo.c.lisboa@gmail.com>
2023-03-18 12:29:50 -0300

    chore: Indentation

--- tmgit.sh
+++ tmgit.sh
@@ -8,17 +8,17 @@
 # Organizar a ordem em que as outras funções são chamadas
 function main() {
 
-    # Realizar verificações de ambiente
-    fm_preflight "$@"
+  # Realizar verificações de ambiente
+  fm_preflight "$@"
     
-    # Preparar ambiente para a execução do programa
-    fm_climb "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
+  # Preparar ambiente para a execução do programa
+  fm_climb "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
     
-    # Realizar as operações que compreendem a lógica central do projeto
-    fm_fly "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
+  # Realizar as operações que compreendem a lógica central do projeto
+  fm_fly "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
 
-    # Realizar tratamentos de erro e geração de logs
-    fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
+  # Realizar tratamentos de erro e geração de logs
+  fm_land "${LAND_ERRLVL}" "${LAND_CALLER}" "${LAND_MSG}" "${LAND_ERRMSG}"
 }
 
 # Carregar o arquivo que abstrai as funções o modo-avião
@@ -26,14 +26,15 @@ function main() {
 
 for file in fm_{preflight,climb,fly,land}.sh
 do
-    echo -ne "Importando arquivo ${file}: "
-    if source "$(dirname "${0}")/${file}" > /dev/null 2>&1
-    then
-        echo -e "OK"
-    else
-        echo -e "FALHOU\n"
-        exit 1
-    fi
+  echo -ne "Importando arquivo ${file}: "
+  if source "$(dirname "${0}")/${file}" > /dev/null 2>&1
+  then
+    echo -e "OK"
+  else
+    echo -e "FALHOU\n"
+    exit 1
+  fi
 done
 
 main "$@"
+
